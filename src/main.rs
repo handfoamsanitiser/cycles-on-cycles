c9165eaf3aa9bb446f5bb8740c6f62d76fb9bce2
Alexey Maleev <alexey.v.maleev@gmail.com>
2024-07-21 08:57:21 +0100

    turned off filtering for clear pixel art

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,41 @@
-// Bevy code commonly triggers these lints and they may be important signals
-// about code quality. They are sometimes hard to avoid though, and the CI
-// workflow treats them as errors, so this allows them throughout the project.
-// Feel free to delete this line.
 #![allow(clippy::too_many_arguments, clippy::type_complexity)]
 
 use bevy::asset::AssetMetaCheck;
+use bevy::window::WindowResolution;
 use bevy::prelude::*;
+
+const TILE_SIZE: f32 = 16.0;
+const PLAYER_SCALE: f32 = 10.0;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(AssetPlugin {
-            // Wasm builds will check for meta files (that don't exist) if this isn't set.
-            // This causes errors and even panics in web builds on itch.
-            // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
+
             meta_check: AssetMetaCheck::Never,
             ..default()
-        }))
+        }).set(WindowPlugin {
+            primary_window: Some(Window {
+                resolution: WindowResolution::new(800.0, 800.0).with_scale_factor_override(1.0),
+                ..default()
+            }),
+            ..default()
+        }).set(ImagePlugin::default_nearest())
+    )
         .add_systems(Startup, setup)
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup(
+    mut commands: Commands, 
+    asset_server: Res<AssetServer>,
+) {
     commands.spawn(Camera2dBundle::default());
     commands.spawn(SpriteBundle {
-        texture: asset_server.load("ducky.png"),
-        ..Default::default()
+        texture: asset_server.load("howard.png"),
+        sprite: Sprite {
+            custom_size: Some(Vec2::new(TILE_SIZE * PLAYER_SCALE, TILE_SIZE * PLAYER_SCALE)),
+            ..default()
+        },
+        ..default()
     });
 }
